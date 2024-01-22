fbeb7fd94c2bacbc007c3ba590b6c954f6a7264d
Hovhannes <Hovhannes.software.engineer@gmail.com>
2024-01-22 07:42:34 +0400

    @Frontend
    * OutDoor
    
    - Integration required packages
    
    - small work on template

--- frontend/outdoor/src/app/page.tsx
+++ frontend/outdoor/src/app/page.tsx
@@ -1,95 +1,36 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+import "./scss/main.scss";
+import "../components/AppBar";
+import { AppBar } from "../components/AppBar";
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main>
+      <AppBar />
+      <div className="lending">
+        <p className="title">PLATFORM PROVIDES PLACE TO LEARN OR TEACH IN WWW</p>
+        <ul className="professions">
+          <h1>PROFESSIONS</h1>
+          <ul>
+            <li>Javascript Developer</li>
+            <li>PHP Developer</li>
+            <li>Web Developer</li>
+            <li>Graphic Designer</li>
+            <li>...</li>
+            {/* <li>Business Analyst</li>
+            <li>Content Writer</li>
+            <li>Android Developer</li>
+            <li>Swift Developer</li>
+            <li>Go Developer</li>
+            <li>C# Developer</li> */}
+          </ul>
+        </ul>
+        <ul className="couches">
+          <h1>COUCHES</h1>
+          <ul>
+          <li>Coming Soon</li>
+          </ul>
+          
+        </ul>
       </div>
     </main>
-  )
+  );
 }
